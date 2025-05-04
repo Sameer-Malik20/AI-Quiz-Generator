6c516461844f801dd236115be1b353a0eb2b2da9
Sameer-Malik20 <sameermalik63901@gmail.com>
2025-05-04 16:03:33 +0530

    Update quizapp.py

--- quizapp.py
+++ quizapp.py
@@ -67,5 +67,6 @@ def generate_quiz():
     except Exception as e:
         return jsonify({"error": f"Unexpected Error: {str(e)}"}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
