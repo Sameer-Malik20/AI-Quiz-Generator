a61a76864e3840d4ef2730a887eea936326969fc
Sameer-Malik20 <sameermalik63901@gmail.com>
2025-05-04 18:07:29 +0530

    Update quizapp.py

--- quizapp.py
+++ quizapp.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-client = genai.Client(api_key=os.getenv("GENAI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 @app.route('/generate-quiz', methods=['POST'])
 def generate_quiz():
